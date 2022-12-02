380275c6615bc1e6f7dd502129f7ac1d7ad8fb73
Takasur Azeem <takasurazeem@gmail.com>
2022-12-02 20:28:31 +0500

    Adds pets end-point
    The pets end-point sends either a single component or a list of components, it's an array that can have 1 or more components
    
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 const express = require('express')
 const app = express() 
 
-// localhost:3000
-app.get('/', (req, res) => {
-    res.json({message: 'Hello World'})
+// pet-listing
+app.get('/pet-listing', (req, res) => {
+    const model = {
+        pageTitle: 'Pets',
+        components: [
+            {
+                type: 'featuredImage',
+                data: {
+                    imageUrl: "https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+                }
+            }
+        ]
+    }
+    res.json(model)
 })
 
 app.listen(3000, () => {
